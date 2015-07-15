b56fcaaf5d997131275da83c7abe1cf7b534cc31
Paul <vgycsk@gmail.com>
2015-07-15 18:03:52 +0800

    Update pod framework

--- Pods/Headers/Public/TMTumblrSDK/TMSDKUserAgent.h
+++ Pods/Headers/Public/TMTumblrSDK/TMSDKUserAgent.h
@@ -1,15 +1,1 @@
-//
-//  TMSDKUserAgent.h
-//  TMTumblrSDK
-//
-//  Created by Bryan Irace on 12/17/14.
-//  Copyright (c) 2014 Tumblr. All rights reserved.
-//
-
-#import <Foundation/Foundation.h>
-
-@interface TMSDKUserAgent : NSObject
-
-+ (NSString *)userAgentHeaderString;
-
-@end
+../../../TMTumblrSDK/TMTumblrSDK/Core/TMSDKUserAgent.h
