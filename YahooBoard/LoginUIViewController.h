cd5ea1487fe0a176b116ec1ec8900675f271822d
Paul <vgycsk@gmail.com>
2015-07-17 12:02:02 +0800

    Added login view

---
+++ YahooBoard/LoginUIViewController.h
@@ -0,0 +1,13 @@
+//
+//  LoginUIViewController.h
+//  YahooBoard
+//
+//  Created by Shu-Yen Chang on 7/17/15.
+//  Copyright (c) 2015 XXX. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface LoginUIViewController : UIViewController
+
+@end
