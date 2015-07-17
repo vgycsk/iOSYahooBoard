66180750805a5a7340d2a10ffdcb85bc86c51a41
YD LIN <cyme@yahoo-inc.com>
2015-07-17 10:18:15 +0800

    init tumblr page

---
+++ YahooBoard/TumblrDetailViewController.h
@@ -0,0 +1,15 @@
+//
+//  TumblrDetailViewController.h
+//  YahooBoard
+//
+//  Created by Yi-De Lin on 7/17/15.
+//  Copyright (c) 2015 XXX. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface TumblrDetailViewController : UIViewController
+@property (weak, nonatomic) IBOutlet UIImageView *image;
+@property (weak, nonatomic) IBOutlet UILabel *desc;
+
+@end
---
+++ YahooBoard/TumblrDetailViewController.m
@@ -0,0 +1,13 @@
+//
+//  TumblrDetailViewController.m
+//  YahooBoard
+//
+//  Created by Yi-De Lin on 7/17/15.
+//  Copyright (c) 2015 XXX. All rights reserved.
+//
+
+#import "TumblrDetailViewController.h"
+
+@implementation TumblrDetailViewController
+
+@end
